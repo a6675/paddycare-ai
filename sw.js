2cd36a7c4dfa2ddaf310ede287a70db732e7c54d
a6675 <a6675@ocean.umt.edu.my>
2026-05-12 23:11:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'paddycare-v2';
-const ASSETS = [
+// Fail ini membolehkan app berfungsi offline
+const CACHE_NAME = 'paddy-v1';
+const assets = [
   './',
   './index.html',
-  './manifest.json',
-  './model.json',
-  './model.weights.bin' // Important: cache the heavy AI data!
+  './scanner.html',
+  './logo_paddy.png',
+  './website.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
